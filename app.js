e343f7d031ca8218ec0e6bf3f6319d2e3093464e
tarunnagar8191 <tarunnagar8191@gmail.com>
2023-08-01 09:10:20 +0530

    done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const enq = require("./modals/Enquiry.modal");
 const app = express();
-const port = 8080;
+const port = 8082;
 
 app.use(express.json());
 
